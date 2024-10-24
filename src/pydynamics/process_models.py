a5d32c01b176ea05038395f18364770143177cd5
Ali Zaidi <ali.zaidi@ubc.ca>
2024-10-24 11:59:53 -0700

    add class method for fit diagnostics

--- src/pydynamics/process_models.py
+++ src/pydynamics/process_models.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import namedtuple
 
 from scipy.integrate import odeint
 from scipy.optimize import minimize
 from scipy.interpolate import interp1d
 
+#TODO: Generate Results class with named tuples
 
 class FOPDT():
   def __init__(self, t, y, u):
@@ -60,18 +62,17 @@ class FOPDT():
     self.params_opt = solution.x
     return self.params_opt
   
-  def step(self, params):
+  def step(self, params=None, step=1):
     """
     Generate a step response plot for a FOPDT model with the given params
     """
-    params = self.params_opt
-    Km = params[0]
+    if params is None:
+      params = self.params_opt
     taum = params[1]
-    thetam = params[2]
     ts = np.arange(-taum, 5*taum)
 
     us = np.zeros((len(ts)))
-    us[np.int8(taum)+1:] = 1
+    us[np.int8(taum)+1:] = step
 
     y = self.simulate(params, ts, us)
     plt.figure()
@@ -82,6 +83,7 @@ class FOPDT():
     plt.grid()
     plt.ylabel("Output")
     plt.subplot(212)
+    plt.ylabel("Input")
     plt.plot(ts, us)
     plt.grid()
 
@@ -114,6 +116,40 @@ class FOPDT():
       plt.title("Input")
       plt.xlabel("time")
       plt.ylabel("change in input")
+      plt.draw()
 
     return result
+  
+  # Diagnostics start here
+  def plot_diagnostics(self):
+    yd, ud, td = self.y, self.u, self.t
+    resid = yd - self.result["y_pred"]
+    resid = resid - resid[0]
+    residd = resid.copy()
+    residd[1:] = np.diff(resid)
+    plt.subplots(3,2, figsize=(10,15))
+    plt.subplot(3,2,1)
+    plt.plot(td, resid)
+    plt.title("Residuals")
+    plt.grid()
+    plt.subplot(3,2,2)
+    plt.title("Diff Residuals")
+    plt.plot(td, residd)
+    plt.grid()
+    plt.subplot(3,2,3)
+    plt.title("Autocorrelation Residuals")
+    plt.xcorr(resid, resid, maxlags=30)
+    plt.ylim([-1,1])
+    plt.subplot(3,2,4)
+    plt.title("Autocorrelation diff_Residuals")
+    plt.xcorr(residd, residd, maxlags=30)
+    plt.ylim([-1,1])
+    plt.subplot(3,2,5)
+    plt.title("Cross-correlation Input -> Residuals")
+    plt.xcorr(ud, resid, maxlags=30)
+    plt.ylim([-1,1])
+    plt.subplot(3,2,6)
+    plt.title("Cross-correlation Input -> Diff Residuals")
+    plt.xcorr(ud, residd, maxlags=30)
+    plt.ylim([-1,1])
     
